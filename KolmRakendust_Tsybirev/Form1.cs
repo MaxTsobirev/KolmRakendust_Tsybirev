76fe397889f122c4b8dd3bb8767e0ae93c9143ab
opilane <opilane@HP-CZC6297BB1>
2022-09-28 11:02:26 +0300

    buttons options

--- KolmRakendust_Tsybirev/Form1.cs
+++ KolmRakendust_Tsybirev/Form1.cs
@@ -1,3 +1,4 @@
+using System.Windows.Forms;
 using System.Windows.Forms.Design;
 
 namespace KolmRakendust_Tsybirev
@@ -6,6 +7,12 @@ namespace KolmRakendust_Tsybirev
     {
         TableLayoutPanel table;
         PictureBox pict;
+        OpenFileDialog openFile = new OpenFileDialog
+        {
+            Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All files (*.*)|*.*",
+            Title = "Select a Picture",
+        };
+        ColorDialog colorDialog = new ColorDialog{};
         public Form1()
         {
             this.Size = new Size(800, 600);
@@ -34,12 +41,43 @@ namespace KolmRakendust_Tsybirev
             flowLayoutPanel.Controls.Add(button3);
             flowLayoutPanel.Controls.Add(button4);
 
+            button1.Click += Button1_Click;
+            button2.Click += Button2_Click;
+            button3.Click += Button3_Click;
+            button4.Click += Button4_Click;
+
             table.Controls.Add(pict);
             table.Controls.Add(flowLayoutPanel);
             table.SetCellPosition(flowLayoutPanel, new TableLayoutPanelCellPosition(1, 1));
             this.Controls.Add(table);
+            
         }
 
-        
+        private void Button4_Click(object? sender, EventArgs e)
+        {
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                pict.Load(openFile.FileName);
+            }
+        }
+
+        private void Button3_Click(object? sender, EventArgs e)
+        {
+            pict.Image = null;
+        }
+
+        private void Button2_Click(object? sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+                pict.BackColor = colorDialog.Color;
+        }
+
+        private void Button1_Click(object? sender, EventArgs e)
+        {
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                pict.Load(openFile.FileName);
+            }
+        }
     }
 }
